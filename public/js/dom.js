3bbf78a3f3caa05662dab84eb7ca1278fbd79738
Jehad91 <j.abushaqra1@gmail.com>
2022-03-28 18:34:21 +0300

    Edit User post comments Relates #21

--- public/js/dom.js
+++ public/js/dom.js
@@ -22,6 +22,7 @@ if (document.cookie) {
   userName = document.cookie.split('; ')[0].slice(9);
   userID = document.cookie.split('; ')[1].slice(3);
   navAvatarImg.setAttribute('title', userName);
+  usernameProfile.textContent = userName;
   navAvatarImg.setAttribute('href', `/user/${userID}/profile`);
   loginMainButton.style.display = 'none';
   logoutMainButton.style.display = 'block';
